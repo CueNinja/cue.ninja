e46ebaad17cab8de77aab04fbb94d8294affdce4
Ethan Apodaca <papodaca@gmail.com>
2018-03-30 14:23:20 -0700

    Get querying working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     application
-    kotlin("jvm") version "1.2.30"
+    kotlin("jvm") version "1.2.31"
 }
 application {
     mainClassName = "ninja.cue.Main"
@@ -16,15 +16,13 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib"))
-    compile("no.tornado:tornadofx:1.7.15")
     compile("org.controlsfx:controlsfx:9.0.0")
-    compile("com.github.thomasnield:rxkotlinfx:2.2.2")
-    compile("javax.json:javax.json-api:1.1.2")
     compile("de.jensd:fontawesomefx-commons:9.1.2")
-    compile("de.jensd:shichimifx:1.2.2")
-    compile("ninja.cue:monaco.editor:0.1-SNAPSHOT")
+    compile("de.jensd:fontawesomefx-fontawesome:4.7.0-9.1.2")
+//    compile("de.jensd:shichimifx:1.2.2")
+    compile("ninja.cue:monaco.editor:0.2-SNAPSHOT")
 
     compile("org.postgresql:postgresql:42.2.1")
     //--------------------- Devtools ---------------------
-    compile("ninja.cue:javafx-webview-debugger:0.6.0-SNAPSHOT")
+//    compile("ninja.cue:javafx-webview-debugger:0.6.0-SNAPSHOT")
 }
--- src/main/kotlin/ninja/cue/views/MainWindow.kt
+++ src/main/kotlin/ninja/cue/views/MainWindow.kt
@@ -1,13 +1,24 @@
 package ninja.cue.views
 
+import javafx.beans.property.SimpleStringProperty
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.MenuBar
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
+import javafx.util.Callback
+import java.sql.Connection
+import java.sql.DriverManager
+
 import ninja.cue.monaco.Monaco
+import java.sql.ResultSet
 
 class MainWindow {
     @FXML private var monaco = Monaco()
     @FXML private var mainMenu = MenuBar()
+    @FXML private var table = TableView<Array<String>>()
+
+    private var connection: Connection? = null
 
     fun initialize() {
         mainMenu.useSystemMenuBarProperty().set(true)
@@ -19,6 +30,40 @@ class MainWindow {
 
     @FXML fun debugger(actionEvent: ActionEvent) {
         //monaco.startDebugger()
+    }
+
+    fun makeConnection(): Connection {
+        if(connection == null) {
+            val url = "jdbc:postgresql://localhost/rock?user=usr1"
+            connection = DriverManager.getConnection(url)
+        }
+        return connection!!
+    }
+
+    fun executeQuery(event: ActionEvent) {
+        val query = monaco.getContent()
+        val stmt = makeConnection().createStatement()
+        val results: ResultSet = stmt.executeQuery(query)
+
+        val columns = Array<String>(results.metaData.columnCount) {
+            results.metaData.getColumnName(it + 1)
+        }
+
+        table.columns.setAll(columns.map {
+            val cell = TableColumn<Array<String>, String>(it)
+            val index = columns.indexOf(it)
+            cell.cellValueFactory = Callback { SimpleStringProperty(it.value[index]) }
+            cell
+        })
+
+        table.items.clear()
+
+        while(results.next()) {
+            val row = Array<String>(columns.size) {
+                results.getString(it + 1)
+            }
+            table.items.add(row)
+        }
     }
 
 }
